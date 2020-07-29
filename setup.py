e71b066e4b5176dab7e06069b2afae26c3c03a4b
Adam Young <ayoung@redhat.com>
2020-07-29 16:32:49 -0400

    Starting a simple Django project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='cecksvc',
+   version='1.0',
+   description='A Web service to check connectivity with otheer wed services',
+   author='Adam Young',
+   author_email='ayoung@redhat.com',
+   packages=['checksvc'],
+   install_requires=[], 
+)
